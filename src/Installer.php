eca675a366f4677e745906cef4e0bbf33b712700
epilepticmane <justangrose@gmail.com>
2024-10-16 14:31:03 +0300

    исправил logTemplate теперь в бд верно пишет при insert or delete

--- src/Installer.php
+++ src/Installer.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2024 - 2024, WebHost1, LLC. All rights reserved.
  * Author: epilepticmane
  * File: Installer.php
- * Updated At: 16.10.2024, 14:25
+ * Updated At: 16.10.2024, 14:31
  *
  */
 
@@ -707,13 +707,13 @@ final class Installer
                         if ($subKey === null || $subKey === '') {
                             continue;
                         }
-                        $template = str_replace("%{$subKey}%", "',$operation.$subKey,')", $template);
+                        $template = str_replace("%{$subKey}%", "',$operation.$subKey,'", $template);
                     }
                 } else {
                     if ($value === null || $value === '') {
                         continue;
                     }
-                    $template = str_replace("%{$value}%", "',$operation.$value,')", $template);
+                    $template = str_replace("%{$value}%", "',$operation.$value,'", $template);
                 }
             }
             $template = str_replace('%datePrefix%', date('Y-m-d H:i:s'), $template);
