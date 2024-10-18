f4f840109d56616eb24336929193b412bd72560a
epilepticmane <justangrose@gmail.com>
2024-10-18 14:57:19 +0300

    check 2

--- src/Service/ApiSorm.php
+++ src/Service/ApiSorm.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2024 - 2024, WebHost1, LLC. All rights reserved.
  * Author: epilepticmane
  * File: ApiSorm.php
- * Updated At: 18.10.2024, 14:54
+ * Updated At: 18.10.2024, 14:57
  *
  */
 
@@ -43,21 +43,35 @@ final class ApiSorm extends SormService
                 continue;
             }
 
-            foreach ($keys as $key => $value) {
-                if (is_array($value)) {
-                    foreach ($value as $subKey) {
-                        if ($subKey === null || $subKey === '') {
-                            continue;
-                        }
-                        echo "63: Обращаемся к $tableName.$subKey\n";
-                    }
-                } else {
-                    if($value === null || $value === '') {
-                        continue;
-                    }
-                    echo "69: Обращаемся к $tableName.$value\n";
-                }
+            echo "Обращаемся к БД: $tableName\n";
+
+            $query = "SELECT COUNT(*) as total FROM `$tableName`";
+            $result = $database->prepare($query);
+
+            if ($result) {
+                $count = $result->fetchColumn();
+                echo "Общее количество элементов в $tableName: $count\n";
+            } else {
+                echo "[Error] Не удалось выполнить запрос для таблицы $tableName.\n";
             }
+
+            // Закомментированный код для обращения к ключам, если потребуется
+//        foreach ($keys as $key => $value) {
+//            if (is_array($value)) {
+//                foreach ($value as $subKey) {
+//                    if ($subKey === null || $subKey === '') {
+//                        continue;
+//                    }
+//                    echo "63: Обращаемся к $tableName.$subKey\n";
+//                }
+//            } else {
+//                if ($value === null || $value === '') {
+//                    continue;
+//                }
+//                echo "69: Обращаемся к $tableName.$value\n";
+//            }
+//        }
         }
     }
+
 }
