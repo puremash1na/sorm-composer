b668faf4491220ac316163fe0b799ab7a53333c2
epilepticmane <justangrose@gmail.com>
2024-10-18 15:10:05 +0300

    check6

--- src/Service/ApiSorm.php
+++ src/Service/ApiSorm.php
@@ -3,7 +3,7 @@
  * Copyright (c) 2024 - 2024, WebHost1, LLC. All rights reserved.
  * Author: epilepticmane
  * File: ApiSorm.php
- * Updated At: 18.10.2024, 15:08
+ * Updated At: 18.10.2024, 15:10
  *
  */
 
@@ -43,7 +43,7 @@ final class ApiSorm extends SormService
                 continue;
             }
 
-            echo "Обращаемся к БД: $tableName в СОРМЕ: $dbConfig\n";
+            echo "Обращаемся к БД: $tableName в СОРМЕ: $logicalTableName\n";
 //
 //            try {
 //                $query = "SELECT * FROM `$tableName`";
